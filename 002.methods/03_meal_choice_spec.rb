002c2eede9c99eaf3ee19a0abe490dee059affa2
Spencer Rogers <spencer1248@gmail.com>
2013-09-27 12:48:47 -0400

    Fix meat_choice rspec comparison

--- 002.methods/03_meal_choice_spec.rb
+++ 002.methods/03_meal_choice_spec.rb
@@ -3,10 +3,10 @@ require_relative './meal_choice.rb' # Code your solution in this file
 
 describe '#meal_choice' do
   it 'should default to meat' do
-    meal_choice.should be "meat"
+    meal_choice.should == "meat"
   end
 
   it 'should allow you to set a meal' do
-    meal_choice("vegan").should be "vegan"
+    meal_choice("vegan").should == "vegan"
   end
 end
