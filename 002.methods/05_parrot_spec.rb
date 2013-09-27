b3fba36e7468cda609fbd3774a847e9d83008360
Spencer Rogers <spencer1248@gmail.com>
2013-09-27 12:00:11 -0400

    Add 05_parrot_spec.rb to assess output vs. return values.

---
+++ 002.methods/05_parrot_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../spec_helper'
+require_relative './parrot.rb' # Code your solution in this file
+
+describe '#parrot' do
+  it 'should say "Squawk!" by default' do
+    $stdout.should_receive(:puts).with("Squawk!")
+
+    parrot
+  end
+
+  it 'should return the default phrase' do
+    phrase = parrot
+
+    phrase.should == "Squawk!"
+  end
+
+  it 'should output the given phrase' do
+    $stdout.should_receive(:puts).with("Pretty bird!")
+
+    parrot("Pretty bird!")
+  end
+
+  it 'should output the given phrase' do
+    phrase = parrot("Pretty bird!")
+
+    phrase.should == "Pretty bird!"
+  end
+end
