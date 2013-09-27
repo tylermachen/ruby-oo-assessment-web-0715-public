20468a1c4bbb390dc11f841be47637341482da83
Spencer Rogers <spencer1248@gmail.com>
2013-09-27 13:10:07 -0400

    Fix email parser rspec comparisons

--- 004.arrays/01_email_parser_spec.rb
+++ 004.arrays/01_email_parser_spec.rb
@@ -5,25 +5,25 @@ describe '#email_parser' do
   it "parses CSV emails" do
     emails = "avi@test.com, bob@test.com"
 
-    email_parser(emails).should be ["avi@test.com", "bob@test.com"]
+    email_parser(emails).should == ["avi@test.com", "bob@test.com"]
   end
 
   it "parses space delimited emails" do
     emails = "avi@test.com bob@test.com"
 
-    email_parser(emails).should be ["avi@test.com", "bob@test.com"]
+    email_parser(emails).should == ["avi@test.com", "bob@test.com"]
   end
 
   it "parses both CSV and spac delimited emails" do
     emails = "avi@test.com, bob@test.com test@avi.com, test@bob.com"
 
-    email_parser(emails).should be ["avi@test.com", "bob@test.com","test@avi.com", "test@bob.com"] 
+    email_parser(emails).should == ["avi@test.com", "bob@test.com","test@avi.com", "test@bob.com"] 
   end
 
   it 'parses and removes duplicate emails' do
     emails = "avi@test.com, avi@test.com"
     
-    email_parser(emails).should be ["avi@test.com"]
+    email_parser(emails).should == ["avi@test.com"]
   end
 
 end
