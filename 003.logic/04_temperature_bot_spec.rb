8cfaacb6d61478e259b781b5af882b246e8347c3
Spencer Rogers <spencer1248@gmail.com>
2013-09-27 13:06:53 -0400

    Fix temperature bot rspec comparison

--- 003.logic/04_temperature_bot_spec.rb
+++ 003.logic/04_temperature_bot_spec.rb
@@ -3,15 +3,15 @@ require_relative './temperature_bot.rb' # Code your solution in this file
 
 describe '#temperature_bot' do
   it 'is comfortable at room temperature' do
-    temperature_bot(18).should be 'comfortable'
-    temperature_bot(21).should be 'comfortable'
+    temperature_bot(18).should == 'comfortable'
+    temperature_bot(21).should == 'comfortable'
   end
 
   it 'is hot above room temperature' do
-    temperature_bot(22).should be 'hot'
+    temperature_bot(22).should == 'hot'
   end
 
   it 'is cold below room temperature' do
-    temperature_bot(14).should be 'cold'
+    temperature_bot(14).should == 'cold'
   end
 end
