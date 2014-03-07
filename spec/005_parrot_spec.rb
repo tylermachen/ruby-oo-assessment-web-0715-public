5a714275ad4ca8aa673c0b23a3828673aa7cf32c
Spencer Rogers <spencer1248@gmail.com>
2014-03-07 11:55:15 -0500

    Fix parrot spec arrangement.

--- spec/005_parrot_spec.rb
+++ spec/005_parrot_spec.rb
@@ -1,34 +1,39 @@
 require_relative '../spec_helper'
 require_relative '../lib/parrot.rb' # Code your solution in this file
 
-describe Parrot,'#speak' do
-  it 'should return the default phrase' do
-    parrot = Parrot.new
-    parrot.phrase.should == 'Squawk!'
+describe Parrot do
+  
+  describe '#phrase' do
+    it 'should return the default phrase' do
+      parrot = Parrot.new
+      parrot.phrase.should == 'Squawk!'
+    end
+      
+    it 'should return the given phrase' do
+      parrot = Parrot.new
+      parrot.phrase = 'Pretty bird!'
+  
+      parrot.phrase.should == "Pretty bird!"
+    end
   end
 
-  it 'should say "Squawk!" by default' do
-    # $stdout is a global variable that represents the console IO
-    # just like the constant STDOUT
-    $stdout.should_receive(:puts).with("Squawk!")
-
-    Parrot.new.speak
+  describe '#speak' do
+    it 'should say "Squawk!" by default' do
+      # $stdout is a global variable that represents the console IO
+      # just like the constant STDOUT
+      $stdout.should_receive(:puts).with("Squawk!")
+  
+      Parrot.new.speak
+    end
+  
+  
+    it 'should output the given phrase' do
+      parrot = Parrot.new
+      parrot.phrase = "Pretty bird!"
+  
+      $stdout.should_receive(:puts).with("Pretty bird!")
+  
+      parrot.speak
+    end
   end
-
-  it 'should return the given phrase' do
-    parrot = Parrot.new
-    parrot.phrase = 'Pretty bird!'
-
-    parrot.phrase.should == "Pretty bird!"
-  end
-
-  it 'should output the given phrase' do
-    parrot = Parrot.new
-    parrot.phrase = "Pretty bird!"
-
-    $stdout.should_receive(:puts).with("Pretty bird!")
-
-    parrot.speak
-  end
-
 end
